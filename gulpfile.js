be1becdeca595e4181e59e0614071f5b074e521a
nkt054237@gmail.com <nkt054237@gmail.com>
2022-09-03 12:20:24 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function(){
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function(){
+    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+})
+
+gulp.task('default', gulp.parallel('browser-sync', 'styles'));
